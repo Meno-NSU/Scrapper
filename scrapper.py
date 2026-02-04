a69340f145a84876ee8890b7dbfd7f414a20bde3
ThinkingDuck <TriDuch@yandex.ru>
2026-02-04 14:33:43 +0700

    Add configs with entry point script

---
+++ scrapper.py
@@ -0,0 +1,87 @@
+import asyncio
+import datetime
+from pathlib import Path
+from typing import Iterator
+from dotenv import load_dotenv
+import os
+import yaml
+from crawlers import crawl_nsu_vk_knowledge as cvk
+from crawlers import crawl_nsu_web_knowledge as cweb
+import merge_knowledge as mk
+
+def delete_files(files: Iterator[Path]) -> None:
+    for file in files:
+        file.unlink()
+        print(f"\tУдален: {file}")
+
+def _clear_data_before_crawling(directory: Path) -> None:
+    delete_files(directory.rglob('*.jsonl'))
+
+def crawl_vk_data(urls_dir: Path, output_dir: Path, config: dict):
+    token = os.getenv("VK_SERVICE_TOKEN")
+    if token is None:
+        raise ValueError("❌ В .env файле не задан VK_SERVICE_TOKEN")
+    
+    urls_file = urls_dir.joinpath("vk_urls.json")
+    output_file = output_dir.joinpath("vk_scrapped.jsonl")
+
+    cutoff_date = None
+    if (config["VK_CUTOFF_DATE"] is not None):
+        cutoff_date = int(datetime.datetime.strptime(str(config["VK_CUTOFF_DATE"]), "%Y-%m-%d").timestamp())
+
+    cvk.crawl_vk_knowledge(token, urls_file, output_file, cutoff_date)
+    
+async def craw_web_data(urls_dir: Path, output_dir: Path, config: dict):
+    url_fname = urls_dir.joinpath("web_urls.json")
+
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    filename = f"web_scrapped_{current_date}.jsonl"
+    output_file = output_dir.joinpath(filename)
+
+    await cweb.crawl_web_knowledge(url_fname, output_file, cweb.get_configs())
+
+def run_scrapper():
+    load_dotenv()
+    
+    config: dict = dict()
+    default_config: dict = dict()
+    with open('config.yaml', 'r') as f_config,\
+         open('default_config.yaml', 'r') as f_def_config:
+        config = yaml.safe_load(f_config)
+        default_config = yaml.safe_load(f_def_config)
+    
+    if (config.get("scrapper", None) is None):
+        print("Пропускаем Scrapper")
+        return
+    
+    config = default_config["scrapper"] | config["scrapper"]
+
+    BASE = Path().cwd()
+    URLS_DIR = BASE.joinpath(config['URLS_DIR'])
+    OUTPUT_DIR = BASE.joinpath(config['OUTPUT_DIR'])
+    
+    if (config["CLEAR_BEFORE_CRAWL"]):
+        print(f"Очищение {OUTPUT_DIR} от .jsonl перед сбором данных")
+        _clear_data_before_crawling(OUTPUT_DIR)
+
+    print("Сбор данных с ВК...")
+    crawl_vk_data(URLS_DIR, OUTPUT_DIR, config)
+    print("Сбор данных с web-источников...")
+    asyncio.run(craw_web_data(URLS_DIR, OUTPUT_DIR, config))
+
+    BASE = Path().cwd()
+    OUTPUT = OUTPUT_DIR.joinpath("merged_latest_knowledge.jsonl")
+    files_dict = mk.get_latest_files(OUTPUT_DIR)
+
+    mk.merge_jsonl_files(list(files_dict.values()), OUTPUT)
+    if (not config["SAVE_TEMP_FILES"]):
+        print("Удаление временных файлов:")
+        delete_files(iter(files_dict.values()))
+
+
+
+def main():
+    run_scrapper()
+
+if __name__ == "__main__":
+    main()
