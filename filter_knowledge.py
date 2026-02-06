d76836fdd27db7ffd1484c3dd18ba9d1ac79c365
ThinkingDuck <TriDuch@yandex.ru>
2026-02-06 11:53:41 +0700

    Add remove_emojis

--- filter_knowledge.py
+++ filter_knowledge.py
@@ -2,17 +2,40 @@ import json
 from pathlib import Path
 from collections.abc import Callable
 from tqdm import tqdm
+import re
 
-
-def delete_empty_content(item: dict) -> dict | None:
+def _delete_empty_content(item: dict) -> dict | None:
     if not item["content"]:
         return None
 
     return item
 
+def _remove_emojis(item: dict):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    item['content'] = re.sub(emoj, '', item['content'])
+    return item
+
 
 def get_pipeline() -> list[Callable]:
-    return [delete_empty_content]
+    return [_remove_emojis, _delete_empty_content]
 
 
 def process(input_file: Path, output_file: Path, pipeline: list[Callable]):
